9fe4d643d0345552145c94e2f323426ef326e0b4
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-11-09 12:32:30 -0600

    first commit

---
+++ video_code/would_you_rather/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+import {ref} from 'vue'
+
+const wyrQuestion = ref('Would you rather have homemade pizza or homemade pasta?')
+const wyrAnswer1 = ref('Homemade pizza')
+const wyrAnswer2 = ref('Homemade pasta')
+
+const userSelection = ref('')
+
+function updateUserSelection(userChoice){
+    userSelection.value = `I see, you would rather have ${userChoice}.`
+}
+
+</script>
+
+<template>
+<div id="app-components">
+
+<h1>Would You Rather?</h1>
+
+    <WouldYouRather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserSelection"></WouldYouRather>
+
+    <p>{{ userSelection }}</p>
+</div>
+</template>
+
+
+
+<style scoped>
+p{
+    font-family: Arial, Helvetica, sans-serif;
+}
+#app-components{
+    /*font-size: 3em;*/
+    background-color: aqua;
+    padding: 40px;
+}
+</style>
---
+++ video_code/would_you_rather/src/components/WouldYouRather.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const emit = defineEmits([
+  'answer-selected'
+])
+
+const choice = ref('')
+
+//emits event to tell parent that user has made a choice
+// function choiceMade(){
+//   emit('answer-selected', choice.value)
+// }
+
+//if choice has changed, change value in choice
+watch(choice, () =>{
+  emit('answer-selected',choice.value)
+})
+</script>
+
+<template>
+
+<div id="wyr">
+
+    <h2>Make your choice!</h2>
+
+  <p>{{ question }}</p>
+  <div>
+    <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
+    <label for="answer-1">{{ answer1 }}</label>
+    
+    <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+    <label for="answer-2">{{ answer2 }}</label>
+  </div>
+</div>
+  
+</template>
+
+<style scoped>
+#wyr{
+  background-color: lightblue;
+}
+
+p{
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+</style>
